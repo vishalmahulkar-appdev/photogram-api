302d3d8e45d1e86eecc938cfb5500534ed00a984
Vishal Mahulkar <vishal.mahulkar@gmail.com>
2019-11-02 21:28:25 +0000

    half way

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,102 @@
 class ApplicationController < ActionController::Base
+
+  def list_users
+    answer = User.all
+
+    render( {:plain => answer.to_json } )
+  end
+
+  def user_details
+    u = User.where({ :username => params.fetch(:username) }).at(0)
+    if u.nil?
+      answer = "No Record with username: " + params.fetch(:username)
+    else
+      answer = u
+    end
+
+    render( {:plain => answer.to_json } )
+  end
+
+  def user_own_photos
+    u = User.where({ :username => params.fetch(:username) }).at(0)
+    if u.nil?
+      answer = "No Record with username: " + params.fetch(:username)
+    else
+      answer = u.own_photos
+    end
+
+    render( {:plain => answer.to_json } )
+  end
+
+  def user_liked_photos
+    u = User.where({ :username => params.fetch(:username) }).at(0)
+    if u.nil?
+      answer = "No Record with username: " + params.fetch(:username)
+    else
+      answer = u.liked_photos
+    end
+
+    render( {:plain => answer.to_json } )
+  end
+
+  def user_feed
+    u = User.where({ :username => params.fetch(:username) }).at(0)
+    if u.nil?
+      answer = "No Record with username: " + params.fetch(:username)
+    else
+      answer = u.feed
+    end
+
+    render( {:plain => answer.to_json } )
+  end
+
+  def user_discover
+    u = User.where({ :username => params.fetch(:username) }).at(0)
+    if u.nil?
+      answer = "No Record with username: " + params.fetch(:username)
+    else
+      answer = u.discover
+    end
+
+    render( {:plain => answer.to_json } )
+  end
+
+  def photo_details
+    p = Photo.where({ :id => params.fetch(:id) }).at(0)
+    if p.nil?
+      answer = "No Record with photo_id: " + params.fetch(:id)
+    else
+      answer = p
+    end
+
+    render( {:plain => answer.to_json } )
+  end
+
+  def photo_likes
+    p = Photo.where({ :id => params.fetch(:id) }).at(0)
+    if p.nil?
+      answer = "No Record with photo_id: " + params.fetch(:id)
+    else
+      answer = p.likes
+    end
+
+    render( {:plain => answer.to_json } )
+  end
+
+  def photo_fans
+    p = Photo.where({ :id => params.fetch(:id) }).at(0)
+    if p.nil?
+      answer = "No Record with photo_id: " + params.fetch(:id)
+    else
+      answer = p.fans
+    end
+
+    render( {:plain => answer.to_json } )
+  end
+
+  def insert_like_record
+    answer = "working on it"
+    render( {:plain => answer.to_json } )
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,21 @@
 Rails.application.routes.draw do
 
+  # Users
+  match("/users", {:controller => "application", :action => "list_users", :via => "get"} )
+  match("/users/:username", {:controller => "application", :action => "user_details", :via => "get"} )
+  match("/users/:username/own_photos", {:controller => "application", :action => "user_own_photos", :via => "get"} )
+  match("/users/:username/liked_photos", {:controller => "application", :action => "user_liked_photos", :via => "get"} )
+  match("/users/:username/feed", {:controller => "application", :action => "user_feed", :via => "get"} )
+  match("/users/:username/discover", {:controller => "application", :action => "user_discover", :via => "get"} )
+
+  # Photos
+  match("/photos/:id", {:controller => "application", :action => "photo_details", :via => "get"} )
+  match("/photos/:id/likes", {:controller => "application", :action => "photo_likes", :via => "get"} )
+  match("/photos/:id/fans", {:controller => "application", :action => "photo_fans", :via => "get"} )
+
+  #Insert
+  match("/insert_like_record", {:controller => "application", :action => "insert_like_record", :via => "get"} )
+
 
   # =================================================
   devise_for :admin_users, ActiveAdmin::Devise.config
